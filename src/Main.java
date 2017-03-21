2ec35b9154122dc0f082860ade880a4d8fa69d18
Andrew Briasco <andrew_briasco-stewart@student.wayland.k12.ma.us>
2017-03-21 07:18:54 -0400

    Edits for last push

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  */
 
 /**
- * to do: shift the screen based on the position of the player, (use shiftUp and shiftDown commands ->
+ * to do: shift the screen based on the position of the player, (use shift commands ->
  * when player is above or below certain y values)
  * Create platforms on screen and above screen (randomly?)
  * balance gravity and screen shifting
@@ -54,32 +54,10 @@ public class Main extends JPanel {
 
                 player.setOnPlatform(false);
                 for(Platform a: platform){
-                    if(player.intersects(a) && player.getSpeed()<1 && (player.getLoc().y+39) < a.getLoc().y+8) {
+                    if(player.intersects(a) && player.getSpeed()<1 && player.getLoc().y+39 < a.getLoc().y+9) {
                         player.setOnPlatform(true);
 //                        player.setLoc(new Point(player.getLoc().x, a.getLoc().y-38));
                     }
-                }
-
-
-                if(keys[KeyEvent.VK_W]){
-//                    System.out.println("w");
-                    if(player.isOnPlatform()) {
-                        player.jump();
-                        player.setDir(Sprite.NORTH);
-                        player.update();
-                        keys[KeyEvent.VK_W] = false; //probably.
-                        player.setOnPlatform(false);
-                    }
-                }
-                if(keys[KeyEvent.VK_D]){
-//                    System.out.println("d");
-                    player.setLoc(new Point(player.getLoc().x+6, player.getLoc().y));
-                    player.update();
-                }
-                else if(keys[KeyEvent.VK_A]){
-//                    System.out.println("a");
-                    player.setLoc(new Point(player.getLoc().x-6, player.getLoc().y));
-                    player.update();
                 }
 
                 if(!player.isOnPlatform()&& gravity <0) {
@@ -96,6 +74,28 @@ public class Main extends JPanel {
                 //bug testing for gravity
 //                System.out.println(player.getSpeed());
 
+                if(keys[KeyEvent.VK_W]){
+//                    System.out.println("w");
+                    if(player.isOnPlatform()) {
+                        player.jump();
+                        player.setDir(Sprite.NORTH);
+                        player.update();
+                        keys[KeyEvent.VK_W] = false; //probably.
+                        player.setOnPlatform(false);
+                    }
+                }
+                if(keys[KeyEvent.VK_D]){
+//                    System.out.println("d");
+                    player.setLoc(new Point(player.getLoc().x+5, player.getLoc().y));
+                    player.update();
+                }
+                else if(keys[KeyEvent.VK_A]){
+//                    System.out.println("a");
+                    player.setLoc(new Point(player.getLoc().x-5, player.getLoc().y));
+                    player.update();
+                }
+
+
                 //bounds for game
                 if(player.getLoc().x < -40)
                     player.setLoc(new Point(500, player.getLoc().y));
@@ -104,12 +104,13 @@ public class Main extends JPanel {
 
                 //if player falls out of screen
                 if(player.getLoc().y>700)
-                    player.setLoc(home);
+                    timer.stop();
+//                    player.setLoc(home);
                 //move screen based on player position
                 if(player.getLoc().y<400&&player.getSpeed()>0)
-                    shiftUp(player.getSpeed());
+                    shift(player.getSpeed());
                 if(player.getLoc().y>600 && player.getSpeed()<0)
-                    shiftUp(player.getSpeed()-1);
+                    shift(player.getSpeed());
 
 
 
@@ -141,7 +142,7 @@ public class Main extends JPanel {
         });
     }
 
-    public void shiftUp(int num){
+    public void shift(int num){
         for(Platform a: platform) {
             a.setLoc(new Point(a.getLoc().x, a.getLoc().y + num));
 //            player.setLoc(new Point(player.getLoc().x, player.getLoc().y + num));

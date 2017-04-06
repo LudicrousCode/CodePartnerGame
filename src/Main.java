e648ed65e7b8c6ab823b52725cbb13678f813f2f
rachel_chau <rachel_chau@student.wayland.k12.ma.us>
2017-04-06 10:15:49 -0400

    vicotry screen

--- src/Main.java
+++ src/Main.java
@@ -29,13 +29,16 @@ public class Main extends JPanel {
     private Timer timer;
     private int gravity, spawn, not, fly, sboots, level, points, lives;
     private boolean[] keys;
-    private boolean gameOver, cave, grass, sky, start, sunset;
+    private boolean gameOver, cave, grass, sky, start, sunset, win;
 
     private ArrayList<Platform> platform;
     private ArrayList<Sprite> powerups;
     private ArrayList<Bird> bird;
     private ArrayList<KillerPets> killerPets;
     private ArrayList<Sprite> titleDummies;
+
+    private Sprite cake;
+
 
     Player player = new Player();
 
@@ -50,7 +53,6 @@ public class Main extends JPanel {
         killerPets = new ArrayList<KillerPets>();
         titleDummies = new ArrayList<Sprite>();
         gameOver = false;
-        start = false;
         spawn = 0;
         fly = 0;
         sboots = 0;
@@ -59,6 +61,7 @@ public class Main extends JPanel {
         lives = 1000;
 
         titleDummies.add(new Sprite(100, 145, "gudetamaTitle.png"));
+
 
         powerups.add(new Jetpack(100, FRAMEHEIGHT - 100));
 //        powerups.add(new SuperBoots(200, FRAMEHEIGHT - 100));
@@ -286,26 +289,32 @@ public class Main extends JPanel {
 //                } else if(time <= 300){
 //                    time = 0;
 //                }
-                if(points >= 1000 && !cave) {
+                if(points >= 2 && !cave) {
                     cave  = true;
                     level++;
 //                    System.out.println("level ++");
                 }
-                if(points >= 2000 && !grass){
+                if(points >= 4 && !grass){
                     grass = true;
                     level++;
 //                    System.out.println("level ++");
                 }
-                if(points >= 3000 && !sky){
+                if(points >= 6 && !sky){
                     sky = true;
                     level++;
 //                    System.out.println("level ++");
                 }
 
-                if(points >= 10000 && sunset == false){
+                if(points >= 8 && sunset == false){
                     sunset = true;
                     level++;
 //                    System.out.println("level ++");
+                }
+                if(points >= 10){
+                    start = false;
+                    win = true;
+                    cake = new Sprite(FRAMEWIDTH/2 - 150, 100, "cake.png");
+                    timer.stop();
                 }
 
                 player.update();
@@ -415,112 +424,111 @@ public class Main extends JPanel {
         Graphics2D g2 = (Graphics2D)g;
         GradientPaint sky = new GradientPaint(0, 0, Color.WHITE, 0, 0, Color.WHITE);
 
+        if(start) {
 
-        //level 3: clear-blue sky
-        if(level == 1) {
-            Color skytop = new Color(30, 115, 233);
-            Color skybot = new Color(63, 240, 255);
-            sky = new GradientPaint(FRAMEWIDTH, 0, skybot, 0, FRAMEHEIGHT - FRAMEHEIGHT/22, skytop);
-        }
-
-        //level 2: blizzard
-        if(level == 2) {
-            Color skytop = new Color(0, 43, 21);
-            Color skybot = new Color(45, 13, 97);
-            sky = new GradientPaint(FRAMEWIDTH, 0, skybot, 0, FRAMEHEIGHT - FRAMEHEIGHT/22, skytop);
-        }
-
-        //level 3: purple sky
-        if(level == 3) {
-            Color skytop = new Color(96, 17, 233);
-            Color skybot = new Color(226, 163, 233);
-            sky = new GradientPaint(FRAMEWIDTH, 0, skybot, 0, FRAMEHEIGHT - FRAMEHEIGHT/22, skytop);
-        }
-
-        //level 4: night
-        if(level == 4) {
-            Color skytop = new Color(0, 27, 87);
-            Color skybot = new Color(53, 0, 96);
-            sky = new GradientPaint(FRAMEWIDTH, 0, skybot, 0, FRAMEHEIGHT - FRAMEHEIGHT/22, skytop);
-
-        }
-
-        //level 5: sunset
-        if(level == 5) {
-            Color top = new Color(255, 205, 42);
-            Color bot = new Color(174, 71, 226);
-            sky = new GradientPaint(FRAMEWIDTH, 0, bot, 0, FRAMEHEIGHT - FRAMEHEIGHT/22, top);
-
-        }
-
-        g2.setPaint(sky);
-        g2.fillRect(0, 0, FRAMEWIDTH, FRAMEHEIGHT);
-
-        //starz / snow
-        if(level == 4) {
-            for (int i = 0; i < 50; i++) {
-                g2.setColor(Color.YELLOW);
-                g2.fillOval((int) (Math.random() * FRAMEWIDTH), (int) (Math.random() * FRAMEHEIGHT), 3, 3);
+            //level 3: clear-blue sky
+            if (level == 1) {
+                Color skytop = new Color(30, 115, 233);
+                Color skybot = new Color(63, 240, 255);
+                sky = new GradientPaint(FRAMEWIDTH, 0, skybot, 0, FRAMEHEIGHT - FRAMEHEIGHT / 22, skytop);
             }
-        }
-        if(level == 2){
-            for (int i = 0; i < 500; i++) {
-                g2.setColor(Color.WHITE);
-                g2.fillOval((int) (Math.random() * FRAMEWIDTH), (int) (Math.random() * FRAMEHEIGHT), 3, 3);
+
+            //level 2: blizzard
+            if (level == 2) {
+                Color skytop = new Color(0, 43, 21);
+                Color skybot = new Color(45, 13, 97);
+                sky = new GradientPaint(FRAMEWIDTH, 0, skybot, 0, FRAMEHEIGHT - FRAMEHEIGHT / 22, skytop);
             }
-        }
+
+            //level 3: purple sky
+            if (level == 3) {
+                Color skytop = new Color(96, 17, 233);
+                Color skybot = new Color(226, 163, 233);
+                sky = new GradientPaint(FRAMEWIDTH, 0, skybot, 0, FRAMEHEIGHT - FRAMEHEIGHT / 22, skytop);
+            }
+
+            //level 4: night
+            if (level == 4) {
+                Color skytop = new Color(0, 27, 87);
+                Color skybot = new Color(53, 0, 96);
+                sky = new GradientPaint(FRAMEWIDTH, 0, skybot, 0, FRAMEHEIGHT - FRAMEHEIGHT / 22, skytop);
+
+            }
+
+            //level 5: sunset
+            if (level == 5) {
+                Color top = new Color(255, 205, 42);
+                Color bot = new Color(174, 71, 226);
+                sky = new GradientPaint(FRAMEWIDTH, 0, bot, 0, FRAMEHEIGHT - FRAMEHEIGHT / 22, top);
+
+            }
+
+            g2.setPaint(sky);
+            g2.fillRect(0, 0, FRAMEWIDTH, FRAMEHEIGHT);
+
+            //starz / snow
+            if (level == 4) {
+                for (int i = 0; i < 50; i++) {
+                    g2.setColor(Color.YELLOW);
+                    g2.fillOval((int) (Math.random() * FRAMEWIDTH), (int) (Math.random() * FRAMEHEIGHT), 3, 3);
+                }
+            }
+            if (level == 2) {
+                for (int i = 0; i < 500; i++) {
+                    g2.setColor(Color.WHITE);
+                    g2.fillOval((int) (Math.random() * FRAMEWIDTH), (int) (Math.random() * FRAMEHEIGHT), 3, 3);
+                }
+            }
 
 //        g2.drawRect(FRAMEWIDTH-4, FRAMEHEIGHT-4, 2, 1);
-        for (Platform a : platform) {
-            a.draw(g2);
-        }
-
-        for(Bird b : bird){
-            b.draw(g2);
-        }
-        for(Sprite s: powerups) {
-            s.draw(g2);
-        }
-        for(KillerPets kp: killerPets){
-            kp.draw(g2);
-        }
-
-
-
-
-        player.draw(g2);
-
-
-        if (level == 4 || level == 2) {
-            Color color = new Color(0, 0, 0, 133);
-            g2.setColor(color);
-            g2.fillRect(0, 0, FRAMEWIDTH, FRAMEHEIGHT);
-        }
-        g2.setColor(Color.WHITE);
-        g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
-        g2.drawString("Points: " + points, 100, 695);
-        g2.drawString("Lives: " + lives, 10, 695);
-
-        //jetpack fuel + num superboot jumps
-        g2.setColor(Color.WHITE);
-        g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
-        g2.drawString("Jetpack Fuel: " + fly, 200, 695);
-        g2.drawString("Number of Super Jumps: " + sboots, 325, 695);
-
-
-
-        if(fly>0) {
-            g2.setColor(Color.GRAY);
-            for (int i = 0; i < 30; i++) {
-                g2.fillOval((int) (Math.random() * 44 + player.getLoc().x), (int) (Math.random() * 40 + player.getLoc().y + 39), 5, 5);
+            for (Platform a : platform) {
+                a.draw(g2);
             }
-        }
+
+            for (Bird b : bird) {
+                b.draw(g2);
+            }
+            for (Sprite s : powerups) {
+                s.draw(g2);
+            }
+            for (KillerPets kp : killerPets) {
+                kp.draw(g2);
+            }
+
+
+            player.draw(g2);
+
+
+            if (level == 4 || level == 2) {
+                Color color = new Color(0, 0, 0, 133);
+                g2.setColor(color);
+                g2.fillRect(0, 0, FRAMEWIDTH, FRAMEHEIGHT);
+            }
+            g2.setColor(Color.WHITE);
+            g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+            g2.drawString("Points: " + points, 100, 695);
+            g2.drawString("Lives: " + lives, 10, 695);
+
+            //jetpack fuel + num superboot jumps
+            g2.setColor(Color.WHITE);
+            g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+            g2.drawString("Jetpack Fuel: " + fly, 200, 695);
+            g2.drawString("Number of Super Jumps: " + sboots, 325, 695);
+
+
+            if (fly > 0) {
+                g2.setColor(Color.GRAY);
+                for (int i = 0; i < 30; i++) {
+                    g2.fillOval((int) (Math.random() * 44 + player.getLoc().x), (int) (Math.random() * 40 + player.getLoc().y + 39), 5, 5);
+                }
+            }
 
 //        if(sboots>0){
 //            g2.setColor(Color.WHITE);
 //            g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
 //            g2.drawString("Jumps left: " + sboots, 200, 695);
 //        }
+        }
 
 
 
@@ -535,7 +543,6 @@ public class Main extends JPanel {
 
         if(!start){
 //            g2.setColor(new Color(52, 132, 213));
-
             Color top = new Color(255, 205, 42);
             Color bot = new Color(174, 71, 226);
 
@@ -544,44 +551,54 @@ public class Main extends JPanel {
             g2.setPaint(sky);
 
             g2.fillRect(0, 0, FRAMEWIDTH, FRAMEHEIGHT);
-            g2.setColor(new Color(244, 237, 231));
-            g2.setFont(new Font("Comic Sans MS", Font.BOLD, 50));
-            g2.drawString("Gudetama Jump", FRAMEWIDTH / 2 - 190, FRAMEHEIGHT/8);
+
+            if(!win) {
+
+
+                g2.setColor(new Color(244, 237, 231));
+                g2.setFont(new Font("Comic Sans MS", Font.BOLD, 50));
+                g2.drawString("Gudetama Jump", FRAMEWIDTH / 2 - 190, FRAMEHEIGHT / 8);
 
 //            g2.setColor(new Color(244, 237, 231));
-            g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-            g2.drawString("By: Andrew Briasco-Stewart & Rachel Chau", 45, 130);
+                g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+                g2.drawString("By: Andrew Briasco-Stewart & Rachel Chau", 45, 130);
 
-            g2.setFont(new Font("Comic Sans MS", Font.ITALIC, 15));
-            g2.drawString("Gudetama, the egg yoke, has lost his friends, the cake ingredients.", 20, FRAMEHEIGHT/4 + 100);
-            g2.drawString("Gudetama wants to become a part of the cake with his friends.", 30, FRAMEHEIGHT/4 + 125);
-            g2.drawString("Help Gudetama become a part of the cake", 100, FRAMEHEIGHT/4 + 150);
-            g2.drawString("(which is like 403847182374m in the air).", 100, FRAMEHEIGHT/4 + 175);
+                g2.setFont(new Font("Comic Sans MS", Font.ITALIC, 15));
+                g2.drawString("Gudetama, the egg yoke, has lost his friends, the cake ingredients.", 20, FRAMEHEIGHT / 4 + 100);
+                g2.drawString("Gudetama wants to become a part of the cake with his friends.", 30, FRAMEHEIGHT / 4 + 125);
+                g2.drawString("Help Gudetama become a part of the cake", 100, FRAMEHEIGHT / 4 + 150);
+                g2.drawString("(which is like 403847182374m in the air).", 100, FRAMEHEIGHT / 4 + 175);
 
-            g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
-            g2.drawString("Do you accept this mission?", 150, FRAMEHEIGHT/2 + 50);
+                g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
+                g2.drawString("Do you accept this mission?", 150, FRAMEHEIGHT / 2 + 50);
 
-            g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-            g2.drawString("Instructions:", 190, FRAMEHEIGHT/2 + 160);
+                g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+                g2.drawString("Instructions:", 190, FRAMEHEIGHT / 2 + 160);
 
-            g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
-            g2.drawString("w = jump     a = left     d = right", 145, FRAMEHEIGHT/2 + 185);
-            g2.drawString("Collect jetpacks and superboots to soar your way to the top!", 40, FRAMEHEIGHT/2 + 210);
-            g2.drawString("Hop on birds (they'll give you a boost)", 120, FRAMEHEIGHT/2 + 235);
-            g2.drawString("BEWARE! Avoid the raining cats and dogs!", 105, FRAMEHEIGHT/2 + 260);
-            g2.drawString("Collect hearts to regain lives <3", 140, FRAMEHEIGHT/2 + 285);
+                g2.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
+                g2.drawString("w = jump     a = left     d = right", 145, FRAMEHEIGHT / 2 + 185);
+                g2.drawString("Collect jetpacks and superboots to soar your way to the top!", 40, FRAMEHEIGHT / 2 + 210);
+                g2.drawString("Hop on birds (they'll give you a boost)", 120, FRAMEHEIGHT / 2 + 235);
+                g2.drawString("BEWARE! Avoid the raining cats and dogs!", 105, FRAMEHEIGHT / 2 + 260);
+                g2.drawString("Collect hearts to regain lives <3", 140, FRAMEHEIGHT / 2 + 285);
 
-            Rectangle startButton = new Rectangle(FRAMEWIDTH/2 - 105, FRAMEHEIGHT/2 + 60, 105*2, 50);
-            g2.fill(startButton);
-            g2.setColor(new Color(250, 171, 29));
-            g2.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
-            g2.drawString("YES", FRAMEWIDTH / 2 - 40, FRAMEHEIGHT/2 + 100);
+                Rectangle startButton = new Rectangle(FRAMEWIDTH / 2 - 105, FRAMEHEIGHT / 2 + 60, 105 * 2, 50);
+                g2.fill(startButton);
+                g2.setColor(new Color(250, 171, 29));
+                g2.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
+                g2.drawString("YES", FRAMEWIDTH / 2 - 40, FRAMEHEIGHT / 2 + 100);
 
-            for(Sprite td: titleDummies){
-                td.draw(g2);
+                for (Sprite td : titleDummies) {
+                    td.draw(g2);
+                }
+            } else {
+
+                cake.draw(g2);
+                g2.setColor(new Color(244, 237, 231));
+                g2.setFont(new Font("Comic Sans MS", Font.BOLD, 50));
+                g2.drawString("YOU WON", 115, FRAMEHEIGHT/2 + 100);
+
             }
-
-
 
         }
 
